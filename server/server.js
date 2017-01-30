059f5620885bfbe6e51fc15a254518fbdfad6799
GavMan1995 <gavyncaldwell@gmail.com>
2017-01-30 11:47:54 -0700

    made update url

--- server/server.js
+++ server/server.js
@@ -1,12 +1,13 @@
 const express = require('express')
 const bodyParser = require('body-parser')
-let { ObjectID } = require('mongodb')
+const { ObjectID } = require('mongodb')
+const _ = require('lodash')
 
 const {mongoose} = require('./db/mongoose')
 const {Todo} = require('./models/todo')
 const {User} = require('./models/user')
 
-let app = express()
+const app = express()
 const port = process.env.PORT || 3000
 
 app.use(bodyParser.json())
@@ -57,6 +58,31 @@ app.delete('/todos/:id', (req, res) => {
     })
     .catch((err) => {
       res.status(404).send({error: 'Not a valid Id'})
+    })
+})
+
+app.patch('/todos/:id', (req, res) => {
+  let id = req.params.id
+  let body = _.pick(req.body, ['text', 'completed'])
+
+  if (_.isBoolean(body.completed) && body.completed) {
+    body.completedAt = new Date().getTime()
+  } else {
+    body.completed = false
+    body.completedAt = null
+  }
+
+  Todo.findByIdAndUpdate(id,
+    {$set: body},
+    {new: true})
+    .then((todo) => {
+      if (!todo) {
+        return res.status(404).send({error: 'Todo doesnt exist'})
+      }
+      res.send({todo})
+    })
+    .catch((err) => {
+      res.status(400).send()
     })
 })
 
